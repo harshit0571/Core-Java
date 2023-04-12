544870bb94de2a3f30f617ab62761726baefd2f1
harshit0571 <harshitchadha65@gmail.com>
2023-04-12 13:55:09 +0530

    array

---
+++ Array_input.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Array_input {
+    public static void main(String[] args){
+        Scanner inp=new Scanner(System.in);
+        int[] arr=new int[10];
+        for (int i = 0; i < 10; i++) {
+            arr[i]=inp.nextInt();
+        }
+        for (int i = 0; i < 10; i++) {
+            System.out.print(arr[i]+" ");
+        }
+    }
+}
