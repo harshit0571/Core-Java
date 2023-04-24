3f5504faf6ce55289a81c337763fcb40174fe185
harshit0571 <harshitchadha65@gmail.com>
2023-04-24 10:11:08 +0530

    string

---
+++ string.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class string {
+    public static void main(String[] args) {
+        // String str = new String("d");
+        // // String str2 = new String("java");
+        // // String a = "d";
+        // // String b = "d";
+        // // in the above, it represents two objects, str1 and str created inside the
+        // heap
+        // // area which determines duplicate objects created inside the heap area;
+        // String k;
+        // String b;
+
+        // Scanner inp = new Scanner(System.in);
+        // char a = inp.next().charAt(0);
+        // char b = inp.next().charAt(0);
+
+        // if (b.equals(k)) {
+        // System.out.println(true);
+        // } else {
+        // System.out.println(false);
+        // }
+        // in string class creating object with string literal use string constant pool
+        // , the properties of string constant pool is that it does not allow duplicate
+        // objects of string.
+        // the important difference btw string constan pool and heap area is that heap
+        // area allows duplicate
+        // int a = inp.nextInt();
+        // int b = inp.nextInt();
+
+        // System.out.println(System.identityHashCode(a) + " " +
+        // System.identityHashCode(b));
+
+    }
+}
