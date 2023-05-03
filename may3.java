96a3246bcadd8071974a4f2296a99bda47316cd0
harshit0571 <harshitchadha65@gmail.com>
2023-05-03 14:04:29 +0530

    nested try catch

--- may3.java
+++ may3.java
@@ -1,10 +1,28 @@
 public class may3 {
-    public static void main(String[] args) {
+    public static void main(String args[]) {
+        // try {
+        // int[] arr = { 1, 2, 3 };
+        // System.out.println(arr[10]);
+        // } catch (Exception e) {
+        // System.out.println("error");
+        // }
+        // try {
+        // int d = 0;
+        // d = d / d;
+        // } catch (Exception e) {
+        // System.out.println("error");
+        // }
         try {
             int[] arr = { 1, 2, 3 };
             System.out.println(arr[10]);
         } catch (Exception e) {
-            System.out.println("error");
+            try {
+                System.out.println(e + ".");
+                int[] arr = { 1, 2, 3 };
+                System.out.println(arr[9]);
+            } catch (Exception et) {
+                System.out.println("nested error");
+            }
         }
     }
 }
