2d64c511c3687bc0c8f9c1451fe61099760c1581
harshit0571 <harshitchadha65@gmail.com>
2023-05-08 14:31:18 +0530

    exception handling

---
+++ ExceptionHandling.java
@@ -0,0 +1,44 @@
+// // public class ExceptionHandling {
+// //     public static void main(String[] args) {
+
+// //     }
+// // }
+// import java.io.*;
+
+// class Parent {
+//     void msg() {
+//         System.out.println("parent");
+//     } 
+// }
+
+// public class ExceptionHandling extends Parent{
+
+// void msg()throws IOException{
+// System.out.println("Child");
+// }
+// public static void main(String args[]){
+// Parent p=new ExceptionHandling();
+// p.msg();
+// }
+// }
+// Compile time error
+// // exeption handling by method overriding
+
+import java.io.*;
+
+class Parent {
+    void msg() {
+        System.out.println("parent");
+    }
+}
+
+class ExceptionHandling extends Parent {
+    void msg() throws ArithmeticException {
+        System.out.println("child");
+    }
+
+    public static void main(String args[]) {
+        Parent p = new ExceptionHandling();
+        p.msg();
+    }
+}
--- hashmap.java
+++ hashmap.java
@@ -3,10 +3,9 @@ import java.util.*;
 public class hashmap {
     public static void main(String args[]) {
         HashMap<String, Integer> map = new HashMap<>();
-        map.put("india", 123);
         map.put("usa", 12);
         map.put("france", 99);
-        System.out.println(map.containsKey("indi"));
+        map.put("india", 123);
 
     }
 }
