aad6050e410829c22a1562901038dbaf4ac0c156
harshit0571 <harshitchadha65@gmail.com>
2023-05-16 09:28:27 +0530

    left most repeating

---
+++ bin/leftMostRepeating.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class leftMostRepeating {
+    public static void main(String[] args) {
+        Scanner inp = new Scanner(System.in);
+        String s = inp.nextLine();
+        boolean flag = false;
+        for (int i = 0; i < s.length() - 1; i++) {
+            for (int j = i + 1; j < s.length(); j++) {
+                if (s.charAt(i) == s.charAt(j)) {
+                    System.out.println(s.charAt(i));
+                    flag = true;
+                    break;
+                }
+            }
+            if (flag) {
+                break;
+            }
+        }
+    }
+}
