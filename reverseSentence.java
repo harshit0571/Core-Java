a48c510ebac32263fe5f29c7edb2ff023ef45e65
harshit0571 <harshitchadha65@gmail.com>
2023-05-23 10:42:07 +0530

    string reverse

---
+++ reverseSentence.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class reverseSentence {
+    static String rev(String s) {
+        String[] arr = s.split(" ");
+        String ans = "";
+        for (int i = arr.length - 1; i >= 0; i--) {
+            ans = ans + arr[i] + " ";
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        Scanner inp = new Scanner(System.in);
+        String s = inp.nextLine();
+        s = rev(s);
+        System.out.println(s);
+
+    }
+}
