3e1c705b813fd8bd1da2b6359576116dfb2af321
harshit0571 <harshitchadha65@gmail.com>
2023-06-05 20:50:13 +0530

    question 2

--- question1.java
+++ question1.java
@@ -1,17 +1,39 @@
 import java.util.*;
 
 public class question1 {
+    static boolean search(ArrayList<String> arr, String str) {
+        for (int i = 0; i < arr.size(); i++) {
+            if (str.equals(arr.get(i))) {
+                return true;
+            }
+            ;
+        }
+        return false;
+    }
+
     public static void main(String[] args) {
         Scanner inp = new Scanner(System.in);
-        ArrayList<Integer> arr = new ArrayList<>();
+        ArrayList<String> arr = new ArrayList<>();
         int n = inp.nextInt();
+        inp.nextLine();
         for (int index = 0; index < n; index++) {
-            int a = inp.nextInt();
+            String a = inp.nextLine();
             arr.add(a);
         }
-        // for (int i = 0; i < n; i++) {
-        System.out.println(arr);
-        // }
+
+        ArrayList<String> ans = new ArrayList<>();
+        for (int index = 0; index < n; index++) {
+            boolean isThere = search(ans, arr.get(index));
+            System.out.println(isThere);
+
+            if (isThere == false) {
+                ans.add(arr.get(index));
+            }
+        }
+        System.out.println(ans.size());
 
     }
 }
+
+// aman harshit aman
+// ans : aman , harshit,
