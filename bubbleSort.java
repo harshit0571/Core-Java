b816023e516513e37c3fbd5737dab7d4f1dbcee3
Harshit chadha <62325935+harshit0571@users.noreply.github.com>
2023-08-22 12:26:33 +0530

    Create bubbleSort.java

---
+++ bubbleSort.java
@@ -0,0 +1,40 @@
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+import java.util.*;
+
+class HelloWorld {
+    static void bubbleSort(int[] arr){
+        int count=0;
+        for(int i=0;i<arr.length-1;i++){
+            for(int j=0;j<arr.length-1-i;j++){
+                if(arr[j]>arr[j+1]){
+                    int temp=arr[j];
+                    arr[j]=arr[j+1];
+                    arr[j+1]=temp;
+                    count++;
+                }
+            }
+            print(arr);
+        }
+    }
+    
+    
+    static void print(int[] arr){
+        for(int i=0;i<arr.length;i++){
+            System.out.print(arr[i]+" ");
+        }
+        System.out.println();
+    }
+    public static void main(String[] args) {
+        Scanner inp=new Scanner(System.in);
+        int n=inp.nextInt();
+        int[] arr=new int[n];
+        
+        for(int i=0;i<n;i++){
+            arr[i]=inp.nextInt();
+        }
+        
+        bubbleSort(arr);
+        
+    }
+}
