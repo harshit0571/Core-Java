e1d353ac83e5fb1aac40093362ebfc1031b957ce
Harshit chadha <62325935+harshit0571@users.noreply.github.com>
2023-08-24 23:16:21 +0530

    Update CyclicSort.java

--- CyclicSort.java
+++ CyclicSort.java
@@ -1,41 +1,11 @@
-import java.util.*;
-
-class HelloWorld{
-   static int findMaxIndex(int[] arr, int end){
-        int a=arr[0];
-        int ans=0;
-        for(int i=0; i<end;i++){
-            if(arr[i]>a){
-                ans=i;
-            }
-        }
-        return ans;
+static void sort(int[] arr) {
+    int i = 0;
+    while (i < arr.length) {
+          int correct = arr[i] - 1;
+          if (arr[i] != arr[correct]) {
+              swap(arr, i, correct);
+          } else {
+              i++;
+          }
     }
-    public static void main(String[] args){
-        Scanner inp=new Scanner(System.in);
-        int n=inp.nextInt();
-        int[] arr=new int[n];
-        
-        for(int i=0;i<n;i++){
-            arr[i]=inp.nextInt();
-        }
-        
-        for(int i=0;i<n;i++){
-            int last=n-i;
-            int index=findMaxIndex(arr, last);
-                        System.out.println(index+" ");
-
-            int temp=arr[n-i-1];
-            arr[n-i-1]=arr[index];
-            arr[index]=temp;
-
-        }
-        
-        
-        for(int i=0;i<n;i++){
-            System.out.print(arr[i]+" ");
-        }
-        
-        
-    }   
-}
+}}
