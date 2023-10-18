4a7286c5a10ed505e24f5beb4cef4dd880de7d5d
harshit0571 <harshitchadha65@gmail.com>
2023-10-18 12:24:33 +0530

    index of specified pattern using substring

--- IndexOfSPecifiedPattern.java
+++ IndexOfSPecifiedPattern.java
@@ -6,19 +6,28 @@ public class IndexOfSPecifiedPattern {
         String str = inp.nextLine();
         String pat = inp.nextLine();
 
+        // for (int i = 0; i < str.length(); i++) {
+        // if (str.charAt(i) == pat.charAt(0)) {
+        // int k = 0;
+        // boolean flag = true;
+        // int j = i;
+        // while (j < str.length() && k < pat.length()) {
+        // if (str.charAt(j) != pat.charAt(k)) {
+        // flag = false;
+        // }
+        // j++;
+        // k++;
+        // }
+        // if (k >= pat.length() && flag) {
+        // System.out.println(i);
+        // break;
+        // }
+        // }
+        // }
+
         for (int i = 0; i < str.length(); i++) {
-            if (str.charAt(i) == pat.charAt(0)) {
-                int k = 0;
-                boolean flag = true;
-                int j = i;
-                while (j < str.length() && k < pat.length()) {
-                    if (str.charAt(j) != pat.charAt(k)) {
-                        flag = false;
-                    }
-                    j++;
-                    k++;
-                }
-                if (k >= pat.length() && flag) {
+            if (str.charAt(i) == pat.charAt(0) && (i + pat.length() < str.length())) {
+                if (str.substring(i, i + pat.length()).equals(pat)) {
                     System.out.println(i);
                     break;
                 }
