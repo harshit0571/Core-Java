02b88e681ffd024e2242e921159a37e41a07cebc
harshit0571 <harshitchadha65@gmail.com>
2023-10-18 12:08:43 +0530

    string reverse with reserving spaces

---
+++ StringRevSpacesReserve.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class TwoStackArray {
+    public static void main(String[] args) {
+        Scanner inp = new Scanner(System.in);
+        String str = inp.nextLine();
+        String arr[] = str.split(" ");
+        for (int i = 0; i < arr.length; i++) {
+            StringBuilder sb = new StringBuilder(arr[i]);
+            arr[i] = sb.reverse().toString();
+        }
+        String ans = "";
+        for (int i = 0; i < arr.length; i++) {
+            if (i == arr.length - 1) {
+                ans += arr[i];
+            } else {
+                ans += arr[i] + " ";
+            }
+        }
+        System.out.println(ans);
+    }
+}
