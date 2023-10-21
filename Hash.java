6f226cf7f8b37c0a6ca0c314002b7686d97fde4f
harshit0571 <harshitchadha65@gmail.com>
2023-10-21 09:15:16 +0530

    set

---
+++ Hash.java
@@ -0,0 +1,12 @@
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Stack;
+
+public class Hash {
+    public static void main(String[] args) {
+        Scanner inp = new Scanner(System.in);
+        HashSet<Integer> set = new HashSet<>();
+        HashMap<Integer, Character> map = new HashMap<>();
+    }
+}
