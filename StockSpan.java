f827b9e58d7ea514db24eae2b7ac7a67ad17856a
harshit0571 <harshitchadha65@gmail.com>
2023-10-21 11:11:10 +0530

    stock span

---
+++ StockSpan.java
@@ -0,0 +1,21 @@
+public class Solution {
+    public static int[] findStockSpans(int[] arr) {
+        Stack<Integer> st = new Stack<>();
+        int[] ans = new int[arr.length];
+        st.push(0);
+        ans[0] = 1;
+        for (int i = 1; i < arr.length; i++) {
+            while (!st.empty() && arr[i] > arr[st.peek()]) {
+                st.pop();
+            }
+            if (st.empty()) {
+                ans[i] = i + 1;
+            } else {
+                ans[i] = i - st.peek();
+            }
+
+            st.push(i);
+        }
+        return ans;
+    }
+}
