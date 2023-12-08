73fb80b0d92a3026f4676f6adb4600ebfeb8dfb8
harshit0571 <harshitchadha65@gmail.com>
2023-12-08 17:30:44 +0530

    selection sort

--- BuubleSort.java
+++ BuubleSort.java
@@ -14,8 +14,23 @@ public class BuubleSort {
         System.out.println(Arrays.toString(arr));
     }
 
+    static void Selection(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[i] > arr[j]) {
+                    int temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
+        System.out.println(Arrays.toString(arr));
+
+    }
+
     public static void main(String[] args) {
         int arr[] = { 4, 6, 8, 1, 0, 2 };
         Bubble(arr);
+        Selection(arr);
     }
 }
