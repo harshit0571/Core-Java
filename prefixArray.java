9ee4d98452d397ea12dcc8589593147fa931b657
harshit0571 <harshitchadha65@gmail.com>
2024-02-22 09:46:02 +0530

    prefix sum array

--- prefixArray.java
+++ prefixArray.java
@@ -10,17 +10,20 @@ public class prefixArray {
             arr[i] = inp.nextInt();
         }
 
-        HashMap<Integer, Integer> map = new HashMap<>();
-        for (int i = 0; i < arr.length; i++) {
-            if (map.containsKey(arr[i])) {
-                map.put(arr[i], map.get(arr[i]) + 1);
-            } else {
-                map.put(arr[i], 1);
-            }
-        }
-
         for (int i = 0; i < n; i++) {
-            System.out.print(map.get(arr[i]) + " ");
+
         }
+        // HashMap<Integer, Integer> map = new HashMap<>();
+        // for (int i = 0; i < arr.length; i++) {
+        // if (map.containsKey(arr[i])) {
+        // map.put(arr[i], map.get(arr[i]) + 1);
+        // } else {
+        // map.put(arr[i], 1);
+        // }
+        // }
+
+        // for (int i = 0; i < n; i++) {
+        // System.out.print(map.get(arr[i]) + " ");
+        // }
     }
 }
---
+++ prefixSum.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class prefixSum {
+    public static void main(String[] args) {
+        Scanner inp = new Scanner(System.in);
+        int n = inp.nextInt();
+        int[] arr = new int[n];
+        int[] sumArray = new int[n];
+        int sum = 0;
+        for (int i = 0; i < n; i++) {
+            arr[i] = inp.nextInt();
+            sum += arr[i];
+            sumArray[i] = sum;
+        }
+
+        for (int i = 0; i < sumArray.length; i++) {
+            System.out.print(sumArray[i] + " ");
+        }
+
+    }
+}
